85b26a71bef06b178b2a6b12ad6f8aa57ca45bae
neuen <kansapat4921@gmail.com>
2023-10-19 15:55:56 +0700

    Improved readability in Speed.py

--- Speed.py
+++ Speed.py
@@ -1,11 +1,12 @@
 shipLength = input("Enter the length of the ship in meters: ")
 sailTime = input("Enter the time it takes for the ship to sail its own length in the format mm:ss : ")
+mstoknots = 1.94384
 
 def convertToSeconds(sailTime):
     minutes, seconds = map(int, sailTime.split(':'))
     totalSeconds = minutes*60 + seconds
     return totalSeconds
 
-estimatedSpeed = float(shipLength) / convertToSeconds(sailTime) * 1.94384
+estimatedSpeed = float(shipLength) / convertToSeconds(sailTime) * mstoknots
 
 print("Estimated speed is: ", estimatedSpeed, "knots")
