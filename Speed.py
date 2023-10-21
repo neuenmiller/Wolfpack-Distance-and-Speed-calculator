6338866348f82d617992efa8738b9c0213392faf
neuen <kansapat4921@gmail.com>
2023-10-21 18:50:57 +0700

    Fixed angle on bow issues

--- Speed.py
+++ Speed.py
@@ -1,3 +1,5 @@
+import math
+
 shipLength = input("Enter the length of the ship in meters: ")
 sailTime = input("Enter the time it takes for the ship to sail its own length in the format mm:ss : ")
 angleOnBow = input("Enter the angle on the bow in degrees, insert 0 if you want to ignore this: ")
@@ -8,6 +10,8 @@ def convertToSeconds(sailTime):
     totalSeconds = minutes*60 + seconds
     return totalSeconds
 
-estimatedSpeed = float(shipLength) / convertToSeconds(sailTime) * mstoknots * angleOnBow
+angleOnBow = math.cos(math.radians(float(angleOnBow)))
+
+estimatedSpeed = float(shipLength) / convertToSeconds(sailTime) * float(mstoknots) * abs(angleOnBow)
 
 print("Estimated speed is: ", estimatedSpeed, "knots")
