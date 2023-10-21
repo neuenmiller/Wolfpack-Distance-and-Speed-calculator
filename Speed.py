e9e5d1f890bf1fd9dbc195f017c7c3de9d8d8c29
neuen <kansapat4921@gmail.com>
2023-10-21 18:41:17 +0700

    Added angle on bow for speed.py

--- Speed.py
+++ Speed.py
@@ -1,5 +1,6 @@
 shipLength = input("Enter the length of the ship in meters: ")
 sailTime = input("Enter the time it takes for the ship to sail its own length in the format mm:ss : ")
+angleOnBow = input("Enter the angle on the bow in degrees, insert 0 if you want to ignore this: ")
 mstoknots = 1.94384
 
 def convertToSeconds(sailTime):
@@ -7,6 +8,6 @@ def convertToSeconds(sailTime):
     totalSeconds = minutes*60 + seconds
     return totalSeconds
 
-estimatedSpeed = float(shipLength) / convertToSeconds(sailTime) * mstoknots
+estimatedSpeed = float(shipLength) / convertToSeconds(sailTime) * mstoknots * angleOnBow
 
 print("Estimated speed is: ", estimatedSpeed, "knots")
